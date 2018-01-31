073876f5d4b7b4ee7178509ed5c0917ee39279bf
Son <sonpd@avr-solutions.com>
2018-02-01 01:46:14 +0700

    Refractored code

--- VR_DinoWar/Assets/Scripts/Weapons/Spear.cs
+++ VR_DinoWar/Assets/Scripts/Weapons/Spear.cs
@@ -3,130 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
-public class Spear : VRTK_InteractableObject {
+public class Spear : Weapon {
 	
-	private float impactMagnifier = 5;
-	private float collisionForce = 0f;
-	private float maxCollisionForce = 500f;
-	private VRTK_ControllerReference controllerReference;
-
-	private bool inFlight;
-	private Vector3 initialAngle;
-
-	private Rigidbody rb;
-
-	public Collider meleeCollider;
-	public Collider thrownCollider;
-
-	//Mainly use for calculating velocity
-	public Transform spearTip;
-
-	// Use this for initialization
-	void Start () {
-		rb = this.GetComponent<Rigidbody> ();
+	void Start()
+	{
+		Initialize ();
 	}
 
-
-	// Update is called once per frame
-	void Update () {
-		if (inFlight) {
-			transform.eulerAngles -= Vector3.left * .25f;
-			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, initialAngle.y, initialAngle.z);
-			meleeCollider.enabled = false;
-			thrownCollider.enabled = true;
-		} else {
-			meleeCollider.enabled = true;
-			thrownCollider.enabled = false;
-		}
+	void Update()
+	{
+		Loop ();
 	}
-
-	Vector3 previous;
-	float velocity;
 
 	void FixedUpdate()
 	{
-		velocity = ((spearTip.position - previous).magnitude) / Time.deltaTime;
-		previous = spearTip.position;
-
-	//	print ("spearVelocity " +  velocity );
-	}
-
-	public void Thrown()
-	{
-		inFlight = true;
-		initialAngle = transform.eulerAngles;
-	}
-
-	public override void Grabbed(VRTK_InteractGrab grabbingObject)
-	{
-		base.Grabbed(grabbingObject);
-		controllerReference = VRTK_ControllerReference.GetControllerReference(grabbingObject.controllerEvents.gameObject);
-		ResetPosition ();
-	}
-
-	protected override void OnEnable()
-	{
-		base.OnEnable();
-		controllerReference = null;
-		interactableRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
-	}
-
-	public void ResetPosition()
-	{
-		transform.localPosition = Vector3.zero;
-		transform.localEulerAngles = Vector3.zero;
-	}
-
-	private void OnHitSurface(Transform hitSurface)
-	{
-		print ("OnHitSurface" + hitSurface.name);		
-		rb.velocity = Vector3.zero;
-		rb.isKinematic = true;		
-		inFlight = false;
-		transform.parent = hitSurface;
-	}
-
-	private void Reset()
-	{
-		rb.velocity = Vector3.zero;
-		rb.isKinematic = false;
-		inFlight = false;
-	}
-
-	Enemy enemy;
-	// This is used for throwing spear
-	private void OnCollisionEnter(Collision collision)
-	{
-		// This means that the spear is thrown and hit a surface
-		if (inFlight) {
-			OnHitSurface (collision.transform);
-			inFlight = false;
-
-			enemy = collision.transform.root.GetComponent<Enemy> ();
-			// If player indeed hit the enemy
-			if (enemy != null) {
-				enemy.Hit ();
-			}
-		}
-	}
-
-	public float minForce;
-	// use for melee
-	private void OnTriggerEnter(Collider collision)
-	{
-		print ("spearVelocity " +  velocity + " collision " + collision.transform.name);
-		if (VRTK_ControllerReference.IsValid(controllerReference) && IsGrabbed())
-		{
-			//Only applies damage if player physically put some force to weapon
-			if ( velocity> minForce) {
-				enemy = collision.transform.root.GetComponent<Enemy> ();
-				// If player indeed hit the enemy
-				if (enemy != null) {
-					enemy.Hit ();
-				}
-			}
-				
-		}
+		CalculateVelocity ();
 	}
 
 }
---
+++ VR_DinoWar/Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Weapon : VRTK_InteractableObject {
+
+	// Reference to the controller holding this weapon
+	private VRTK_ControllerReference controllerReference;
+	// is the weapon being thrown?
+	private bool inFlight;
+	// initial angle before the weapon is thrown
+	private Vector3 initialAngle;
+	// local rigidbody reference
+	private Rigidbody rb;
+	// local collider reference 
+	public BoxCollider weaponCollider;
+	// melee collider size - for melee uses only
+	public Vector3 meleeScale;
+	// thrown collider size - for when weapon is thrown 
+	public Vector3 thrownScale;
+	//Mainly use for calculating weapon velocity
+	public Transform weaponTip;
+	// Min force player must physically applied to have a valid hit
+	public float minForce;
+
+	// for weapon velocity calculating
+	private Vector3 previous;
+	private float velocity;
+
+	// Cache
+	private Enemy enemy;
+
+	// Use this for initialization
+	protected void Initialize () {
+		rb = this.GetComponent<Rigidbody> ();
+		weaponCollider.isTrigger = true;
+	}
+
+	protected void Loop () {
+		// Physics hack
+		if (inFlight) {
+			transform.eulerAngles -= Vector3.left * .25f;
+			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, initialAngle.y, initialAngle.z);
+		} 
+	}
+
+	// Put this in FixedUpdate()
+	protected void CalculateVelocity()
+	{
+		velocity = ((weaponTip.position - previous).magnitude) / Time.deltaTime;
+		previous = weaponTip.position;
+	}
+
+	// when weapon is thrown
+	public void Thrown()
+	{
+		inFlight = true;
+		initialAngle = transform.eulerAngles;
+
+		weaponCollider.isTrigger = false;
+		weaponCollider.size = thrownScale;
+	}
+
+	// reset position on grab
+	public override void Grabbed(VRTK_InteractGrab grabbingObject)
+	{
+		base.Grabbed(grabbingObject);
+		controllerReference = VRTK_ControllerReference.GetControllerReference(grabbingObject.controllerEvents.gameObject);
+
+		weaponCollider.size = meleeScale;
+		ResetPosition ();
+	}
+
+	public void ResetPosition()
+	{
+		transform.localPosition = Vector3.zero;
+		transform.localEulerAngles = Vector3.zero;
+	}
+
+	// On hit something when thrown
+	private void OnHitSurface(Transform hitSurface)
+	{
+		print ("OnHitSurface" + hitSurface.name);		
+		rb.velocity = Vector3.zero;
+		rb.isKinematic = true;		
+		inFlight = false;
+		transform.parent = hitSurface;
+	}
+
+	// This is used for throwing 
+	private void OnCollisionEnter(Collision collision)
+	{
+		// This means that the weapon is thrown and hit a surface
+		if (inFlight) {
+			OnHitSurface (collision.transform);
+			CheckIfEnemyAndDealDamage (collision.transform);
+			inFlight = false;
+		}
+	}
+
+	// use for melee
+	private void OnTriggerEnter(Collider collision)
+	{
+		print ("spearVelocity " +  velocity + " collision " + collision.transform.name);
+		if (VRTK_ControllerReference.IsValid(controllerReference) && IsGrabbed())
+		{
+			//Only applies damage if player physically put some force to weapon
+			if ( velocity > minForce) {
+				CheckIfEnemyAndDealDamage (collision.transform);
+			}
+		}
+	}
+
+	void CheckIfEnemyAndDealDamage(Transform t)
+	{
+		enemy = t.root.GetComponent<Enemy> ();
+		// If player indeed hit the enemy
+		if (enemy != null) {
+			enemy.Hit ();
+		}
+	}
+
+
+}
